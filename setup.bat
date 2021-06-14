d55d7ae0c5924ac787c7090ec163f44cc33b7520
Ethan <ethanjhem.e@gmail.com>
2021-06-14 17:05:44 +0100

    Correct batch script

--- setup.bat
+++ setup.bat
@@ -1,1 +1,7 @@
-py setup.py
+@echo off
+python -m pip install --upgrade pip
+pip3 install psutil
+pip3 install requests
+echo "Warning! if modules cannot be found please check that you do not have multiple python versions!"
+python3 setup.py
+pause
